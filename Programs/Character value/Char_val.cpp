624b5839a083b4efd6978660fbabf44850671c1e
Anaba Abbas <129278969+Anaba955@users.noreply.github.com>
2023-07-04 23:20:39 +0530

    Create Char_val.cpp

---
+++ Programs/Character value/Char_val.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+  using namespace std;
+  int main()
+  {
+    char x;
+    cin>>x;
+    if(x=='p'||x=='P')
+      cout<<"PrepBytes"<<endl;
+    else if(x=='Z'||x=='z')
+      cout<<"Zenith"<<endl;
+    else if(x=='E'||x=='e')
+      cout<<"Expert Coder"<<endl;
+    else 
+      cout<<"Data Structure"<<endl;
+    
+    return 0;
+  }
