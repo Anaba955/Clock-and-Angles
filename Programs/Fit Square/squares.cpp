c1d768340a5d1361952904100703b8adc1115281
Anaba Abbas <129278969+Anaba955@users.noreply.github.com>
2023-07-21 00:02:41 +0530

    Create squares.cpp

---
+++ Programs/Fit Square/squares.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+  using namespace std;
+  int main()
+  {
+    int T,M,N;
+    cin>>T;
+    while(T--){
+      cin>>M>>N;
+      cout<<(M*N)/2<<endl;
+    }
+    
+    return 0;
+  }
