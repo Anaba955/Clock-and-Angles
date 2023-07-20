a2019728742d936c0125d53239a6f02a8ec809fd
Anaba Abbas <129278969+Anaba955@users.noreply.github.com>
2023-07-21 00:16:50 +0530

    Create print1.cpp

---
+++ Programs/One Pattern/print1.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+  using namespace std;
+  
+  int main()
+  {
+      int N;
+      cin>>N;
+      for(int i=0;i<=5;i++){
+        int j=i;
+          while(j--){
+            cout<<1<<" ";
+          }
+          cout<<endl;
+      }
+    
+    return 0;
+  }
